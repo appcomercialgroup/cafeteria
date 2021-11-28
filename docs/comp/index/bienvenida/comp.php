43dc7c136a502e7ce22b5e1585fe9ac7e64f80ec
caleman9791 <caleman9791@protonmail.com>
2021-11-28 14:06:50 -0400

    ok

---
+++ docs/comp/index/bienvenida/comp.php
@@ -0,0 +1,34 @@
+<section class="tm-welcome-section">
+  <div class="container tm-position-relative">
+    <div class="tm-lights-container">
+      <img alt="Light" class="light light-1" src="img/light.png"/>
+      <img alt="Light" class="light light-2" src="img/light.png"/>
+      <img alt="Light" class="light light-3" src="img/light.png"/>
+    </div>
+    <div class="row tm-welcome-content">
+      <h2 class="white-text tm-handwriting-font tm-welcome-header">
+        <img alt="Line" class="tm-header-line" src="img/header-line.png"/>
+        Welcome To
+        <img alt="Line" class="tm-header-line" src="img/header-line.png"/>
+      </h2>
+      <h2 class="gold-text tm-welcome-header-2">
+        Cafe House
+      </h2>
+      <p class="gray-text tm-welcome-description">
+        Cafe House template is a mobile-friendly responsive
+        <span class="gold-text">
+          Bootstrap v3.3.5
+        </span>
+        layout by
+        <span class="gold-text">
+          templatemo
+        </span>
+        . Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculusnec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+      </p>
+      <a class="tm-more-button tm-more-button-welcome" href="#main">
+        Details
+      </a>
+    </div>
+    <img alt="Table Set" class="tm-table-set img-responsive" src="img/table-set.png"/>
+  </div>
+</section>
