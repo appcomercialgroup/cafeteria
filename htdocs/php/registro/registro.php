39d28e52fb18fdbaed4ae6763e8c804a8933965b
caleman9791 <caleman9791@protonmail.com>
2021-12-03 02:04:45 -0400

    PHP

---
+++ htdocs/php/registro/registro.php
@@ -0,0 +1,19 @@
+<?php
+
+function FunctionName()
+{
+   $arr = array(
+      'nombre'        => $_POST['nombre'],
+      'usuario'       => $_POST['usuario'],
+      'correo'        => $_POST['correo'],
+      'pass'          => $_POST['pass'],
+      'confirma_pass' => $_POST['confirma_pass'],
+      'pregunta'      => $_POST['pregunta'],
+      'respuesta'     => $_POST['respuesta'],
+   );
+
+   echo json_encode($arr);
+
+}
+
+FunctionName();
