e74bae100ceb07f98a88fe2420706da9c150dc42
caleman9791 <caleman9791@protonmail.com>
2021-12-03 02:48:03 -0400

    Coneccion cambiada contraseña y guardada fuera del proioyecto :)

--- htdocs/conn/conn.php
+++ htdocs/conn/conn.php
@@ -4,27 +4,3 @@ $servername = "localhost";
 $username   = "root";
 $password   = "";
 $dbname     = "app_cafeteria_001";
-
-// $servername = "sql110.epizy.com";
-// $username   = "epiz_30111787";
-// $password   = "Q2DMPYslb3D0Zm";
-// $dbname     = "epiz_30111787_app_cafeteria_001";
-
-// $epiz = array(
-//    "servername" => "sql110.epizy.com",
-//    "username"   => "epiz_30111787",
-//    "password"   => "Q2DMPYslb3D0Zm",
-//    "dbname"     => "epiz_30111787_app_cafeteria_001",
-// );
-
-// $localhost = array(
-//    "servername" => "localhost",
-//    "username"   => "root",
-//    "password"   => "",
-//    "dbname"     => "app_cafeteria_001",
-// );
-
-// function get_conn($arr)
-// {
-//    // code...
-// }
