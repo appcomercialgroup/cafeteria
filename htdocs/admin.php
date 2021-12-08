c24ba7898e10ed0ebe16fe37d28eda4228811298
caleman9791 <caleman9791@protonmail.com>
2021-12-08 04:37:04 -0400

    Ficheros en la raiz de la aplicacion

---
+++ htdocs/admin.php
@@ -0,0 +1,3 @@
+<?php
+
+include "./comp/admin/page.php";
--- htdocs/test.php
+++ htdocs/test.php
@@ -1,15 +1,1 @@
 <?php
-
-include "./db/Conn.php";
-
-$coneccion = new Conn;
-$coneccion->set_conn();
-
-if (condition) {
-   // code...
-}
-// if ($coneccion) {
-//    echo "Conectado";
-// } else {
-//    echo "Fallo la coneccion";
-// }
