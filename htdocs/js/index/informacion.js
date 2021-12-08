ce3081402f32d30cda52b4262ffb78789db9b5b1
caleman9791 <caleman9791@protonmail.com>
2021-12-08 04:33:14 -0400

    Directorio para el javascript de la aplicacion

--- htdocs/js/index/informacion.js
+++ htdocs/js/index/informacion.js
@@ -3,6 +3,13 @@ function informacion_index(argument) {
 	let btn_modal_close = document.querySelector(".btn_modal_close");
 	let mas_informacion = document.querySelector(".mas_informacion");
 	let mobile_menu_icon = document.querySelector(".mobile-menu-icon");
+
+	let btn_informacion = document.querySelectorAll(".btn_informacion");
+	for (var i = 0; i < btn_informacion.length; i++) {
+		btn_informacion[i].addEventListener("click", function(argument) {
+			console.log(this);
+		});
+	}
 	btn_modal_open.addEventListener("click", function(argument) {
 		mas_informacion.style.display = "block";
 	});
--- htdocs/js/inicio_sesion/registro/registro.js
+++ htdocs/js/inicio_sesion/registro/registro.js
@@ -27,8 +27,20 @@
   let txt_pregunta = document.querySelector(".modal_registro .pregunta");
   let txt_respuesta = document.querySelector(".modal_registro .respuesta");
   let btn_enviar = document.querySelector(".modal_registro .btn_enviar");
+  let alerta_mensaje = document.querySelector(".alerta_mensaje");
+  let btn_aceptar_alerta_mensaje =
+    document.querySelector(".modal_registro .btn_aceptar_alerta_mensaje");
+
+  btn_aceptar_alerta_mensaje.style.display = "none";
+  alerta_mensaje.style.display = "none";
+  // let cerar_alerta_mensaje = document.querySelector(".alerta_mensaje span");
+
+  // cerar_alerta_mensaje.addEventListener("click", function(argument) {
+  //   alerta_mensaje.style.display = "none";
+  // });
 
   btn_enviar.addEventListener("click", function function_name(argument) {
+    alerta_mensaje.style.display = "none";
     let datos_form = {
       nombre: fun_valida_texto(txt_nombre.value),
       usuario: fun_valida_texto(txt_usuario.value),
@@ -41,26 +53,33 @@
 
     if (datos_form.nombre) {
       console.log('exito nombre');
+      console.log(txt_nombre.value);
     }
     if (datos_form.usuario) {
       console.log('exito usuario');
+      console.log(txt_usuario.value);
     }
     if (datos_form.correo) {
       console.log('exito correo');
+      console.log(txt_correo.value);
     }
     if (datos_form.pass) {
       console.log('exito pass');
+      console.log(txt_pass.value);
     }
     if (datos_form.confirma_pass) {
       console.log('exito confirma_pass');
+      console.log(txt_confirma_pass.value);
     }
     if (datos_form.pregunta) {
       console.log('exito pregunta');
+      console.log(txt_pregunta.value);
     }
     if (datos_form.respuesta) {
       console.log('exito respuesta');
+      console.log(txt_respuesta.value);
     }
-
+    console.log(datos_form);
     if (
       datos_form.nombre &&
       datos_form.usuario &&
@@ -75,13 +94,13 @@
         txt_usuario,
         txt_correo,
         txt_pass,
-        txt_pregunta,
         txt_confirma_pass,
+        txt_pregunta,
         txt_respuesta
       );
 
     } else {
-      console.log('NO se envia ');
+      console.log('No se envia ');
       console.log('=======================================');
     }
 
@@ -148,19 +167,92 @@
       "respuesta", respuesta.value
     );
 
-    fetch('./php/registro/registro.php', {
-        method: 'POST',
-        body: formData,
-      })
-      .then(response => response.json())
-      .then(result => {
-        console.log('Success:', result);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+    // fetch('./php/registro/registro.php', {
+    //     method: 'POST',
+    //     body: formData,
+    //   })
+    //   .then(response => response.json())
+    //   .then(result => {
+    //     console.log('Success:', result);
+    //   })
+    //   .catch(error => {
+    //     console.error('Error:', error);
+    //   });
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", './php/registro/registro.php', true);
+
+    //Send the proper header information along with the request
+    // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() { // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        console.log(xhr.response);
+        if (JSON.parse(xhr.response)) {
+
+          console.log(JSON.parse(xhr.response));
+
+          let datos = JSON.parse(xhr.response);
+
+          if (datos.respuesta == "existe") {
+
+            console.log('Usuario existe');
+
+            let alerta_mensaje = document.querySelector(".alerta_mensaje");
+            let alerta_mensaje_h3 = document.querySelector(".alerta_mensaje h3");
+            let alerta_mensaje_p = document.querySelector(".alerta_mensaje p");
+            alerta_mensaje.classList.remove("w3-pale-green");
+            alerta_mensaje.classList.add("w3-pale-red");
+            alerta_mensaje_h3.textContent = "Alerta!";
+            alerta_mensaje_p.textContent =
+              "Este usuario o el correo ya existe, prueba otro o recupera tu contraseña!";
+
+            alerta_mensaje.style.display = "block";
+
+          } else if (datos.respuesta == "exito") {
+
+            console.log('Insertado con éxito');
+
+            let alerta_mensaje = document.querySelector(".alerta_mensaje");
+            let alerta_mensaje_h3 = document.querySelector(".alerta_mensaje h3");
+            let alerta_mensaje_p = document.querySelector(".alerta_mensaje p");
+
+            alerta_mensaje.classList.add("w3-pale-green");
+            alerta_mensaje.classList.remove("w3-pale-red");
+            alerta_mensaje_h3.textContent = "Prefecto!";
+            alerta_mensaje_p.textContent = "Bienvenido a la aplicación de mi cafetería!";
+            alerta_mensaje.style.display = "block";
+            let all_btn_modal_registro =
+              document.querySelectorAll(".modal_registro input, .modal_registro a");
+
+            let formulario_registro = document.querySelector(".formulario_registro");
+            formulario_registro.classList.add("form_trasicion");
+            for (var i = 0; i < all_btn_modal_registro.length; i++) {
+
+              all_btn_modal_registro[i].disabled = true;
+            }
+
+            let btn_aceptar_alerta_mensaje =
+              document.querySelector(".modal_registro .btn_aceptar_alerta_mensaje");
+            btn_aceptar_alerta_mensaje.disabled = false;
+            btn_aceptar_alerta_mensaje.style.display = "block";
+
+          } else if (datos.respuesta == "error_datos") {
+
+            console.log('Los datos ingresados no son correctos');
+          }
+
+        } else {
+
+          console.log('Error de respuesta');
+        }
+      }
+    }
+    xhr.send(formData);
+
   }
 
   console.log('registro');
  }
+
  fun_modal_registro();
