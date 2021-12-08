1312bafc40414afd01454037c91b07353a7d9669
caleman9791 <caleman9791@protonmail.com>
2021-12-08 04:30:35 -0400

    Directorio para la apguina de contacto

---
+++ htdocs/comp/contacto/header/comp.php
@@ -0,0 +1,26 @@
+<section class="tm-welcome-section">
+  <div class="container tm-position-relative">
+    <div class="tm-lights-container">
+    <!--   <img alt="Light" class="light light-1" src="img/light.png"/>
+      <img alt="Light" class="light light-2" src="img/light.png"/>
+      <img alt="Light" class="light light-3" src="img/light.png"/> -->
+    </div>
+    <div class="row tm-welcome-content">
+      <h2 class="white-text tm-handwriting-font tm-welcome-header">
+        <img alt="Line" class="tm-header-line" src="img/header-line.png"/>
+       Bienvenido al
+        <img alt="Line" class="tm-header-line" src="img/header-line.png"/>
+      </h2>
+      <h2 class="gold-text tm-welcome-header-2">
+      Panel de administrador
+      </h2>
+      <p class="gray-text tm-welcome-description">
+   Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut temporibus distinctio quasi expedita ipsa laudantium molestiae cumque dolore enim officiis perspiciatis laboriosam iure, ducimus quaerat assumenda deserunt esse voluptas.
+      </p>
+      <a class="pointer tm-more-button tm-more-button-welcome btn_modal_open btn_informacion">
+        Saber mas
+      </a>
+    </div>
+    <img alt="Table Set" class="tm-table-set img-responsive" src="img/table-set.png"/>
+  </div>
+</section>
