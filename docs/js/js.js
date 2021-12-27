3cb0afece47b708e303204b7544715d743d88f96
caleman9791 <caleman9791@protonmail.com>
2021-12-27 03:38:32 -0400

    ok

---
+++ docs/js/js.js
@@ -0,0 +1,15 @@
+var mas_informacion = document.querySelector(".mas_informacion");
+var btn_cerar_modal = document.querySelector(".btn_cerar_modal");
+var tiempo = 20000;
+btn_cerar_modal.addEventListener("click", function(argument) {
+	mas_informacion.style.display = "none";
+
+});
+
+setInterval(function() {
+	console.log('ok');
+	console.log(tiempo);
+	mas_informacion.style.display = "block";
+}, tiempo);
+
+// console.log('ok');
