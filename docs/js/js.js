502cb159dc05178ec032f64608925f69c28eab12
caleman9791 <caleman9791@protonmail.com>
2021-12-27 19:56:04 -0400

    ok

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,16 +1,31 @@
 var mas_informacion = document.querySelector(".mas_informacion");
 var btn_cerar_modal = document.querySelector(".btn_cerar_modal");
-var tiempo = 20000;
+// var tiempo = 20000;
 btn_cerar_modal.addEventListener("click", function(event) {
 	event.preventDefault();
 	mas_informacion.style.display = "none";
 
 });
 
-setInterval(function() {
-	console.log('ok');
-	console.log(tiempo);
-	mas_informacion.style.display = "block";
-}, tiempo);
+var a = document.querySelectorAll("a");
+console.log(a);
+for (var i = 0; i < a.length; i++) {
+
+	a[i].addEventListener("click", function(event) {
+		event.preventDefault();
+		if (this == btn_cerar_modal) {
+			mas_informacion.style.display = "none";
+		} else {
+			mas_informacion.style.display = "block";
+		}
+
+	});
+}
+
+// setInterval(function() {
+// 	console.log('ok');
+// 	console.log(tiempo);
+// 	mas_informacion.style.display = "block";
+// }, tiempo);
 
 // console.log('ok');
