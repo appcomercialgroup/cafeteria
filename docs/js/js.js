711a2da039e6d6d4e63c97158728ae61e8370a8c
caleman9791 <caleman9791@protonmail.com>
2021-12-27 03:42:15 -0400

    ok

--- docs/js/js.js
+++ docs/js/js.js
@@ -1,6 +1,6 @@
 var mas_informacion = document.querySelector(".mas_informacion");
 var btn_cerar_modal = document.querySelector(".btn_cerar_modal");
-var tiempo = 20000;
+var tiempo = 10000;
 btn_cerar_modal.addEventListener("click", function(argument) {
 	mas_informacion.style.display = "none";
 
