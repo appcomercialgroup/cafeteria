4a8d1c995e995efb089da9431f401f7fd7c04023
caleman9791 <caleman9791@protonmail.com>
2022-01-03 01:47:31 -0400

    Conecion local

--- htdocs/conn/conn.php
+++ htdocs/conn/conn.php
@@ -4,23 +4,3 @@ $servername = "localhost";
 $username   = "root";
 $password   = "";
 $dbname     = "app_cafeteria_001";
-
-function conecxion()
-{
-   $servername = "localhost";
-   $username   = "root";
-   $password   = "";
-   $dbname     = "app_cafeteria_001";
-   try {
-
-      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-      // set the PDO error mode to exception
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      return $conn;
-
-   } catch (PDOException $e) {
-      return $e;
-   }
-
-}
